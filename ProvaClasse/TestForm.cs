6fbf3db0f280693ce89b0a0c2139bee6e72f1f9c
KarolChale <karolchale@hotmail.com>
2020-11-23 20:33:01 +0100

    Lista con imagenes sin XLDOCUMENT :v

--- ProvaClasse/TestForm.cs
+++ ProvaClasse/TestForm.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows.Forms;
 using System.Xml.Linq;
+using System.Drawing;
 
 namespace ProvaClasse
 {
@@ -15,24 +16,59 @@ namespace ProvaClasse
 
         private void TestForm_Load(object sender, EventArgs e)
         {
+
+            
             //Alumnos
-            XElement alumnes = null;
-            alumnes = XElement.Load(@"../alumnes.xml");
+            XElement documemt = null;
+            documemt = XElement.Load(@"../img/blueprintimages/Info.xml");
 
 
-            var llistaAlumnes = alumnes.Elements("alumne");
+            var llistaAlumnes = documemt.Elements("TechnicalInfo").Elements("InfoDetails").Elements("InfoDetail");
             foreach (XElement ele in llistaAlumnes)
-                txtResultat.Text += ele.Element("nom") + "\r\n";
+                txtResultat.Text += ele + "\r\n";
 
 
-            //Modulos
-            XElement modulos = null;
-            modulos = XElement.Load(@"../alumnes.xml");
+            // SEGUNDA PARTE
+            var llistaModulos = documemt.Elements("TechnicalInfo").Elements("InfoOptions").Elements("InfoOption");
+            
 
+            getImages();
 
-            var llistaModulos = alumnes.Elements("alumne").Elements("moduls").Elements("modul");
+            listView1.View = View.LargeIcon;
+            imageList1.ImageSize = new Size(32, 32);
+            listView1.LargeImageList = imageList1;
+
+            int imageCount = 0;
+
             foreach (XElement ele in llistaModulos)
-                txtModuls.Text += ele.Element("nomModul").Value + "\r\n";
+            {
+                listView1.Items.Add(new ListViewItem { ImageIndex = imageCount, Text = ele.Element("textOption").Value });
+                txtModuls.Text += ele.Element("textOption").Value + "\r\n";
+                imageCount++;
+            }
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public void getImages()
+        {
+            String ruta = "../img/blueprintimages/iconShips/";
+            String imagepath = "";
+
+            XElement imagenes = null;
+            imagenes = XElement.Load(@"../img/blueprintimages/Info.xml");
+
+
+            var llistaImagenes = imagenes.Elements("TechnicalInfo").Elements("InfoOptions").Elements("InfoOption");
+            foreach (XElement ele in llistaImagenes)
+            {
+                imagepath = ele.Element("icon").Value;
+                imageList1.Images.Add(Image.FromFile(ruta + imagepath));
+            }
 
         }
     }
