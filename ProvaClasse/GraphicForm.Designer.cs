dd31be9eee9a0dcc6bf94424d7f6081967a704af
polmontolio <72408101+polmontolio@users.noreply.github.com>
2020-12-09 20:47:28 +0100

    openfiledialog no eraaaaaaaa!!!! rich textbox ok

--- ProvaClasse/GraphicForm.Designer.cs
+++ ProvaClasse/GraphicForm.Designer.cs
@@ -28,53 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.listView1 = new System.Windows.Forms.ListView();
+            this.rtxt_datos = new System.Windows.Forms.RichTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // chart1
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(45, 118);
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(51, 148);
+            this.chart1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.chart1.Name = "chart1";
-            series1.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.LeftRight;
-            series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series1.IsVisibleInLegend = false;
-            series1.Legend = "Legend1";
-            series1.Name = "ch_Power";
-            this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(735, 421);
+            series4.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.LeftRight;
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.IsVisibleInLegend = false;
+            series4.Legend = "Legend1";
+            series4.Name = "ch_Power";
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(827, 526);
             this.chart1.TabIndex = 13;
             this.chart1.Text = "chart1";
             // 
             // listView1
             // 
             this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(811, 118);
+            this.listView1.Location = new System.Drawing.Point(913, 148);
+            this.listView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(216, 421);
+            this.listView1.Size = new System.Drawing.Size(187, 525);
             this.listView1.TabIndex = 14;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
             // 
+            // rtxt_datos
+            // 
+            this.rtxt_datos.Location = new System.Drawing.Point(1147, 148);
+            this.rtxt_datos.Name = "rtxt_datos";
+            this.rtxt_datos.Size = new System.Drawing.Size(315, 525);
+            this.rtxt_datos.TabIndex = 15;
+            this.rtxt_datos.Text = "";
+            // 
             // GraphicForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.ClientSize = new System.Drawing.Size(1092, 577);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.ClientSize = new System.Drawing.Size(1496, 774);
+            this.Controls.Add(this.rtxt_datos);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.chart1);
+            this.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
             this.Name = "GraphicForm";
             this.Load += new System.EventHandler(this.GraphicForm_Load);
             this.Controls.SetChildIndex(this.chart1, 0);
             this.Controls.SetChildIndex(this.listView1, 0);
+            this.Controls.SetChildIndex(this.rtxt_datos, 0);
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
 
@@ -84,5 +98,6 @@
 
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.RichTextBox rtxt_datos;
     }
 }
--- ProvaClasse/GraphicForm.cs
+++ ProvaClasse/GraphicForm.cs
@@ -2,12 +2,15 @@
 using System.Windows.Forms;
 using System.Drawing;
 using DataManagement;
+using System.IO;
 
 namespace ProvaClasse
 {
     public partial class GraphicForm : Control_Library.BaseForm
     {
         StringData StringData = new StringData();
+        OpenFileDialog ofd = new OpenFileDialog();
+
         private PointF[] puntosFormula = new PointF[1001];
         public GraphicForm()
         {
@@ -18,6 +21,10 @@ namespace ProvaClasse
         {
             Calculo();
             ShowListView(puntosFormula);
+
+            string s = File.ReadAllText("../DLL/testing.log");
+            rtxt_datos.Text = s;
+
         }
 
         private void Calculo()
