01ab30eb7977cebf6366e8e2a579cf2e5a38e0ab
KarolChale <karolchale@hotmail.com>
2021-04-15 19:29:58 +0200

    Modificado mapa

--- BalisesActives/Naivgation.Designer.cs
+++ BalisesActives/Naivgation.Designer.cs
@@ -29,24 +29,14 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Naivgation));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.pictureBox1.Location = new System.Drawing.Point(3, 6);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1022, 885);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(1162, 34);
+            this.button1.Location = new System.Drawing.Point(1245, 122);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
@@ -54,27 +44,47 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1001, 897);
+            this.panel1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1111, 122);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 3;
+            // 
             // Naivgation
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.ClientSize = new System.Drawing.Size(1506, 897);
+            this.ClientSize = new System.Drawing.Size(1766, 897);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.pictureBox1);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Naivgation";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Naivgation";
             this.Load += new System.EventHandler(this.Naivgation_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- BalisesActives/Naivgation.cs
+++ BalisesActives/Naivgation.cs
@@ -26,24 +26,32 @@ namespace BalisesActives
         public string rutaImg = "../img/Bali/";
         private void Naivgation_Load(object sender, EventArgs e)
         {
-
-            heightPic = pictureBox1.Size.Height;
-            widthPic = pictureBox1.Size.Width;
+            
+            heightPic = panel1.Size.Height;
+            widthPic = panel1.Size.Width;
             unitH = heightPic / 21;
             unitW = widthPic / 23;
-            
-            Console.WriteLine("width: " + unitW.ToString() + " height: " + unitH.ToString());
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            PictureBox pbox = new PictureBox();
-            pbox.BackColor = Color.Red;
-            pbox.Name = "pbox_1";
-            pbox.Size = new Size(30, 30);
-            pbox.Location = new Point((int)(unitH * 10), (int)(unitW * 15));
-            this.Controls.Add(pbox);
+            panel1.Controls.Clear();
+            Panel pnl = new Panel();
+            pnl.BackColor = Color.Red;
+            pnl.Name = "pnl_1";
+            pnl.Size = new Size(30, 30);
+            string cadena = textBox1.Text;
+            int numH = getWordNumber(cadena.Substring(0,1).ToUpper());
+            int numW = int.Parse(cadena.Substring(1, cadena.Length - 1)) - 1;
+            pnl.Location = new Point((int)(unitH * numH) + 1, (int)(unitW * numW) - 3);
+            panel1.Controls.Add(pnl);
+        }
 
+        private int getWordNumber(String cadena)
+        {
+            char mychar = cadena[0];
+            int numero = (int)mychar - 65;
+            return numero;
         }
     }
 
